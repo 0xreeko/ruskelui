0fda05cf4fc99eaeb5c4e9b3e32d93209981d089
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-26 13:24:31 +0100

    updated SideNav

--- src/SideNav.tsx
+++ src/SideNav.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import clsx from 'clsx'
 
-export const RuiSideNav = () => {
+export const RuiSideNav = (children) => {
+    const [open, setOpen] = useState(false)
     return (
-        <div className="">this is Sidenav</div>
+        <aside className='inset-0 flex'>
+            <div className={clsx("h-screen bg-russian-400 duration-500 relative", `${open ? "w-64" : "w-20"}`)} >
+                <div className={clsx(`p-1 absolute cursor-pointer object-cover bg-black text-sylver-100 rounded-full -right-3 top-12 duration-300 transform transition-all`, `${open ? `rotate-90` : `-rotate-90`}`)} onClick={() => setOpen(!open)}>
+                    <svg className={clsx(`w-6 h-6`)} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
+                </div>
+                <div className="flex flex-col justify-center px-4 py-6">
+                    herllo
+                </div>
+            </div>
+            <div className={clsx(`flex-1 h-screen`, `${open ? `bg-russian-400/30 duration-300 opacity-100` : `duration-300 opacity-75`}`)}></div>
+        </aside>
     )
 }
