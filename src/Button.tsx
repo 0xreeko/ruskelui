6203ed7cfa95ac35d597e59cdf76293b48cad306
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-26 19:41:25 +0100

    updates

--- src/Button.tsx
+++ src/Button.tsx
@@ -17,6 +17,8 @@ export interface Props extends HTMLAttributes<HTMLButtonElement>{
     
 }
 
+
+
 export const RuiButton = ({label, color = 'amethyst', size = "base", ...props}: Props) => {
   return (
     <button {...props} className={`${color ? `${styles[color]} text-white inline-flex cursor-pointer w-fit items-center transition-all duration-200` : 'bg-gray-700'} ${size === 'base' ? 'mobile-small px-3 py-2 h-9 rounded-[10px]' : size === 'small' ? 'text-[11.1px] px-2 h-8 rounded-[8px]' : size === 'large' ? 'desktop-base px-4 py-3 h-10 rounded-[12px]' : size === 'xlarge' ? 'mobile-large px-5 py-4 h-11 rounded-[14px]' : ''}`}>
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,8 +1,22 @@
 import clsx from 'clsx';
-import React, { HTMLAttributes, useState } from 'react'
+import React, { HTMLAttributes, ReactNode, useState } from 'react'
+// @ts-ignore
+import styles from './modules/Select.module.css'
 
 export interface Props extends HTMLAttributes<HTMLElement> {
     label: string
+}
+
+interface Child extends HTMLAttributes<HTMLElement> {
+    children: ReactNode
+}
+
+export const SelectItem = ({children}: Child) => {
+    return (
+        <li tabIndex={0} className={clsx(styles.selectItem)}>
+            {children}
+        </li>
+    )
 }
 
 export const RuiSelect = ({label = "Choose an ISO20022 coin"}: Props) => {
@@ -14,12 +28,22 @@ export const RuiSelect = ({label = "Choose an ISO20022 coin"}: Props) => {
                 <span>{label}</span>
                 <svg className={clsx(`w-4 h-4`, `${hidden ? 'rotate-0 duration-300' : 'rotate-180 duration-300'}`)} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
             </button>
-            <ul className={clsx(`absolute text-[1rem] w-full z-10 mt-1.5 p-3 h-40 rounded-2xl overflow-scroll bg-russian-500`, `${hidden ? 'transform transition-all -scale-0 opacity-0 duration-300' : 'transform transition-all opacity-100 scale-100 duration-300'}`)} role="listbox">
-                <li className="py-2 pl-4 rounded-lg hover:bg-russian-300/10">XRP</li>
-                <li className="py-2 pl-4 rounded-lg hover:bg-russian-300/10">ALGO</li>
-                <li className="py-2 pl-4 rounded-lg hover:bg-russian-300/10">XLM</li>
-                <li className="py-2 pl-4 rounded-lg hover:bg-russian-300/10">IOTA</li>
-                <li className="py-2 pl-4 rounded-lg hover:bg-russian-300/10">XDC</li>
+            <ul tabIndex={0} className={clsx(`absolute text-[1rem] w-full z-10 mt-1.5 p-3 h-40 rounded-2xl overflow-scroll bg-russian-500`, `${hidden ? 'transform transition-all -scale-0 opacity-0 duration-300' : 'transform transition-all opacity-100 scale-100 duration-300'}`)} role="listbox">
+                <SelectItem>
+                    XRP
+                </SelectItem>
+                <SelectItem>
+                    XLM
+                </SelectItem>
+                <SelectItem>
+                    ALGO
+                </SelectItem>
+                <SelectItem>
+                    IOTA
+                </SelectItem>
+                <SelectItem>
+                    XDC
+                </SelectItem>
             </ul>
         </div >
         // <select name="" id="">
@@ -31,3 +55,7 @@ export const RuiSelect = ({label = "Choose an ISO20022 coin"}: Props) => {
         // </select>
     )
 }
+
+RuiSelect.Item = SelectItem
+
+export default RuiSelect.Item
