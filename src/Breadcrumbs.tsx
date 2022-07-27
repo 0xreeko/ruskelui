a829ee444987dab0136b43dc3ade1593bdae0e65
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-27 09:24:48 +0100

    added WYSIWYG

--- src/Breadcrumbs.tsx
+++ src/Breadcrumbs.tsx
@@ -5,12 +5,21 @@ import styles from './modules/Breadcrumbs.module.css'
 export const RuiBreadcrumbs = () => {
 
     return (
-        <div className="font-sans text-d-copy">
-            <ul className={styles.wrap}>
-                <li className={styles.inactive}><a href="#" className={`before:content-['']`}>Home</a></li>
-                <li className={styles.inactive}><a href="#" className={`before:content-['']`}>Projects</a></li>
+        <nav className={styles.wrapper}>
+            <ol className={styles.innerWrap}>
+                <li><a href="#" className={`text-blue-600 hover:text-blue-700`}>Home</a></li>
+                <li><a href="#" className={`text-blue-600 hover:text-blue-700`}>Projects</a></li>
                 <li>Mirmo Dashboard</li>
-            </ul>
-        </div>
+            </ol>
+        </nav>
+        //         <nav class="bg-grey-light rounded-md w-full">
+        //   <ol class="list-reset flex">
+        //     <li><a href="#" class="text-blue-600 hover:text-blue-700">Home</a></li>
+        //     <li><span class="text-gray-500 mx-2"></span></li>
+        //     <li><a href="#" class="text-blue-600 hover:text-blue-700">Library</a></li>
+        //     <li><span class="text-gray-500 mx-2"></span></li>
+        //     <li class="text-gray-500">Data</li>
+        //   </ol>
+        // </nav> 
     )
 }
