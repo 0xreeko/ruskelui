21549a1443f366ac179532a50fb74395ce74f88d
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-07-27 19:58:21 +0100

    subified Dropdown - included Tester to visualise development

--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -1,28 +1,34 @@
 import clsx from 'clsx';
 import React, { HTMLAttributes, ReactNode, useState } from 'react'
-import { RuiButton } from './Button';
 
 
 export interface Props extends HTMLAttributes<HTMLElement> {
-    label: string
-    id: string
-    route: string
+    label?: ReactNode
+    id?: string
+    route?: string
+    children?: ReactNode
+    leftIcon?: ReactNode
+    rightIcon?: ReactNode
 }
 
 export interface LayoutProps extends HTMLAttributes<HTMLElement> {
     children: ReactNode
 }
 
-export const DropdownItem = ({ route, id, label }: Props) => {
+export const DropdownItem = ({ route, id, children, leftIcon, rightIcon }: Props) => {
     return (
-        <a href={`${route}`} className="block px-4 py-2 text-sm" role="menuitem" tabIndex={-1} id={`menu-item-${id}`}>{label}</a>
+        <a href={route ? `${route}` : ''} className="inline-flex items-center w-full px-2 py-2 text-sm rounded-md hover:bg-russian-400/20" role="menuitem" tabIndex={-1} id={id ? `menu-item-${id}` : ''}>
+            {leftIcon && <span className='inline-flex items-center w-4 h-4 mr-2'>{leftIcon}</span>}
+            {children}
+            {rightIcon && <span className='inline-flex items-center w-4 h-4 ml-auto'>{rightIcon}</span>}
+            </a>
     )
 }
 
-export const DropdownMenu = ({ children, className }: {children: ReactNode, className: string} ) => {
+export const DropdownMenu = ({ children, className }: {children: ReactNode, className?: string} ) => {
     return (
         <div className={clsx(`origin-center absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-russian-600 text-sylver-100 ring-1 ring-black ring-opacity-5 focus:outline-none`, `${className}`)} role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex={-1}>
-            <div className={clsx(`py-1`)} role="none">
+            <div className={clsx(`py-2.5 px-2.5`)} role="none">
                 {children}
             </div>
         </div>
@@ -31,29 +37,24 @@ export const DropdownMenu = ({ children, className }: {children: ReactNode, clas
 }
 
 
-export const RuiDropdown = ({ label = "Options" }: Props) => {
+export const RuiDropdown = ({ label = "Options", children }: Props) => {
     const [hidden, setHidden] = useState(true)
 
     return (
         <div className="relative inline-block text-left">
-            <div onClick={() => setHidden(!hidden)}>
-                <RuiButton label={`${label}`} color='russian' size='small' />
-            </div>
+                <button onClick={() => setHidden(!hidden)} className='w-14 h-8 flex items-center justify-center px-4 py-2.5 rounded-lg appearance-none min-w-fit text-d-copy duration-300 hover:bg-russian-600/20 text-sylver-800'>
+                    {label}
+                </button>
 
             <DropdownMenu className={`${hidden ? `transition ease-in duration-75 transform opacity-0 scale-95` : `transition ease-out duration-300 transform opacity-100 scale-100`}`}>
-                {
-                    [{ route: "#", label: "Account Settings" }, { route: "#", label: "Support" }, { route: "#", label: "License" }].map(({ label, route }, idx) => (
-                        <DropdownItem route={route} id={`${idx}`} label={label} />
-                    ))
-                }
-                <form method="POST" action="#" role="none">
-                    <button type="submit" className="block w-full px-4 py-2 text-sm text-left text-gray-700" role="menuitem" tabIndex={-1} id="menu-item-3">Sign out</button>
-                </form>
+                {children}
             </DropdownMenu>
         </div>
     )
 }
 
 // RuiDropdown.Button = RuiDropdownButton
+RuiDropdown.Menu = DropdownMenu
+RuiDropdown.Item = DropdownItem
 
 export default RuiDropdown
