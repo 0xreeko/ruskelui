d5e21feb88ecd1117e97b624987c6cb5ea3027b8
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-09 16:54:49 +0100

    modded tw config to match the required config for dev usage

--- configs/tailwind.config.js
+++ configs/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontSize: {
