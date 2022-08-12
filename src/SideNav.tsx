c19ba66a9e82c183f0966b0c60b92c0b6b7b8689
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-08-12 18:11:24 +0100

    modded Button txt wrapping bug, SideNav to the correct minimised width

--- src/SideNav.tsx
+++ src/SideNav.tsx
@@ -4,7 +4,7 @@ export const RuiSideNav = () => {
     const [open, setOpen] = useState(false)
     return (
         <aside className='inset-0 flex'>
-            <div className={`h-screen bg-russian-400 duration-500 relative ${open ? "w-64" : "w-20"}`} >
+            <div className={`h-screen bg-russian-400 duration-500 relative ${open ? "w-72" : "w-20"}`} >
                 <div className={`p-1 absolute cursor-pointer object-cover bg-black text-sylver-100 rounded-full -right-3 top-12 duration-300 transform transition-all ${open ? `rotate-90` : `-rotate-90`}`} onClick={() => setOpen(!open)}>
                     <svg className={`w-6 h-6`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg>
                 </div>
