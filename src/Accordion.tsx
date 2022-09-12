03b05a7ca8f2329a51413e515a48e2353164ed9b
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-09-12 08:27:37 +0100

    modded Accordion structure and props, and tweaked styles in the case of multi liner in Item label

--- src/Accordion.tsx
+++ src/Accordion.tsx
@@ -1,62 +1,76 @@
-import React, { HTMLAttributes, useState } from 'react'
+import React, { HTMLAttributes, ReactNode, useState } from 'react'
 // @ts-ignore
 import styles from './modules/Accordion.module.css'
-
-
-interface AccordionData {
-    title: string
-    content: string
-}
+import { colorType } from './utils'
 
 export interface Props extends HTMLAttributes<HTMLElement> {
-    data: AccordionData[]
-    open: boolean
-    onClick: () => void
+    label?: ReactNode
+    content?: ReactNode
+    children?: ReactNode
+    color?: colorType
 }
 
-
-const AccordionHeader = () => {
-    return(
-        <div className="">hey</div>
-    )
+const hoverColor: { [key in colorType]: string } = {
+    amber: "hover:text-amber-400 duration-300",
+    amethyst: "hover:text-amethyst-400 duration-300",
+    aqua: "hover:text-aqua-400 duration-300",
+    azure: "hover:text-azure-400 duration-300",
+    blue: "hover:text-blue-400 duration-300",
+    cyan: "hover:text-cyan-400 duration-300",
+    emerald: "hover:text-emerald-400 duration-300",
+    fuchsia: "hover:text-fuchsia-400 duration-300",
+    green: "hover:text-green-400 duration-300",
+    indigo: "hover:text-indigo-400 duration-300",
+    jade: "hover:text-jade-400 duration-300",
+    jasper: "hover:text-jasper-400 duration-300",
+    kunzite: "hover:text-kunzite-400 duration-300",
+    lime: "hover:text-lime-400 duration-300",
+    moonstone: "hover:text-moonstone-400 duration-300",
+    orange: "hover:text-orange-400 duration-300",
+    peridot: "hover:text-peridot-400 duration-300",
+    pink: "hover:text-pink-400 duration-300",
+    purple: "hover:text-purple-400 duration-300",
+    red: "hover:text-red-400 duration-300",
+    rose: "hover:text-rose-400 duration-300",
+    ruby: "hover:text-ruby-400 duration-300",
+    russian: "hover:text-russian-400 duration-300",
+    scarlet: "hover:text-scarlet-400 duration-300",
+    stratos: "hover:text-stratos-400 duration-300",
+    sunstone: "hover:text-sunstone-400 duration-300",
+    sylver: "hover:text-sylver-400 duration-300",
+    teal: "hover:text-teal-400 duration-300",
+    violet: "hover:text-violet-400 duration-300",
+    yellow: "hover:text-yellow-400 duration-300"
 }
-const AccordionBody = () => {
-    return(
-        <div className="">this is my body</div>
-    )
-}
 
-export const RuiAccordion = ({ open, data = [{ title: "What is Web3?", content: "Web3 is a new paradigm!" }, { title: "What is XRP?", content: "The world's bridge currency👁" }] }: Props) => {
+const AccordionItem = ({ label, content, color = "amethyst"}: Props) => {
     const [hidden, setHidden] = useState<boolean | number | null>(true);
+    return (
+        <>
+            <button className={`${styles.accordionItem} ${hoverColor[color]}`} onClick={() => setHidden(!hidden)}>
+                <span className='text-left text-d-base'>{label}</span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" className={`w-4 h-4 ${hoverColor[color]} ${hidden === true ? `${styles.iconBase}` : `${styles.iconActive}`}`} width={24} height={24} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                    <line x1={12} y1={5} x2={12} y2={19}></line>
+                    <line x1={5} y1={12} x2={19} y2={12}></line>
+                </svg></span>
+            </button>
+            <p className={` ${styles.content} ${hidden === true ? `${styles.contentHidden}` : `${styles.contentVisible}`}`}>{content}</p>
+        </>
+    )
+}
 
-    const toggle = (idx: number) => {
-        hidden === idx ? setHidden(null) : setHidden(idx)
-    }
+export const RuiAccordion = ({ children }: Props) => {
 
     return (
         <div className={`${styles.wrapper}`} tabIndex={0}>
-            {data.map((item, idx) => (
-                <>
-                    <button className={`${styles.accordionItem}`} onClick={() => toggle(idx)}>
-                        <span>{item.title}</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" className={`w-4 h-4 duration-300 ${hidden !== idx ? `${styles.iconBase}` : `${styles.iconActive}`}`} width={24} height={24} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <line x1={12} y1={5} x2={12} y2={19}></line>
-                            <line x1={5} y1={12} x2={19} y2={12}></line>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" className={`w-4 h-4 duration-300 ${hidden !== idx ? `hidden` : `flex`}`} width={24} height={24} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                            <line x1={5} y1={12} x2={19} y2={12}></line>
-                        </svg>
-                    </button>
-                    <p className={` ${styles.content} ${hidden !== idx ? `${styles.contentHidden}` : `${styles.contentVisible}`}`}>{item.content}</p>
-                </>
-            ))}
+            {children}
         </div>
     )
 }
 
-RuiAccordion.Header = AccordionHeader
-RuiAccordion.Body = AccordionBody
+RuiAccordion.Item = AccordionItem
 
 export default RuiAccordion
+
+
--- stories/Accordion.stories.tsx
+++ stories/Accordion.stories.tsx
@@ -1,6 +1,6 @@
-import {Meta, Story} from '@storybook/react'
+import { Meta, Story } from '@storybook/react'
 import React from 'react';
-import {RuiAccordion, Props} from '../src/Accordion'
+import { RuiAccordion, Props } from '../src/Accordion'
 
 const meta: Meta = {
     title: "@RuskelUI/Components/Accordion",
@@ -9,6 +9,19 @@ const meta: Meta = {
 
 export default meta
 
-const Template: Story<Props> = args => <RuiAccordion {...args} />
+
+const _data = [
+    { label: "What is RuskelUI?", content: "v" },
+    { label: "What is Web3Source that opens the portal?", content: "v" },
+    { label: "What is Thirdflow?", content: "v" },
+]
+
+const Template: Story<Props> = args => <RuiAccordion>
+    {
+        _data.map(({ label, content }) =>
+            <RuiAccordion.Item label={label} content={content} />
+        )
+    }
+</RuiAccordion>
 
 export const Default = Template.bind({})
