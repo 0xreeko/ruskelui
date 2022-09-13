e92be9180424323ffd1bf44334dabb96e91bca25
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-09-13 10:29:37 +0100

    modded rui tw config for class based LDM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,mdx}', './stories/*'],
-  darkMode: 'media',
+  darkMode: 'class',
   theme: {
     extend: {
       fontSize: {
