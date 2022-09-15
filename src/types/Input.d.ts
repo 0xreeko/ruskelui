de3c556fe1a020153195b2394dadadc1ac41294e
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-09-15 20:59:47 +0100

    created Input type with dummy props

---
+++ src/types/Input.d.ts
@@ -0,0 +1,9 @@
+interface RuiInputProps {
+    label: '' //string
+    placeholder: '' // string
+    floatingLabel: '' // string
+    fullWidth: '' // bool
+    color: '' // color
+    leftIcon: '' // ReactNode
+    rightIcon: '' // ReactNode
+}
