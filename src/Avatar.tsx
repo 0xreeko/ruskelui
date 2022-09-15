c1e7996bc3eb38e566b1c0d4578b0ae74ceeae98
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-09-16 00:35:50 +0100

    modded Avatar props

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -1,24 +1,13 @@
-import React, { HTMLAttributes } from 'react'
-import { avatarSize, colorType } from './utils'
+import React from 'react'
+import { RuiAvatarProps } from './types/Avatar'
 // @ts-ignore
 import styles from './modules/Avatar.module.css'
 
-export interface Props extends HTMLAttributes<HTMLImageElement> {
-    /** Choose one the scalar options for Avatar */
-    size: avatarSize
-    /** Provide a radius type for Avatar */
-    rounded: 'base' | 'full'
-    /** Provide an image for Avatar */
-    src: string
-    /** Provide an alt for Avatar's image */
-    alt: string
-    badgeColor?: colorType
-}
 
-export const RuiAvatar = ({ size = 6, rounded = 'base', src, alt }: Props) => {
+export const RuiAvatar = ({ size = 6, variant = 'base', src, alt }: RuiAvatarProps) => {
     return (
         <div className='relative w-fit'>
-            <div className={`overflow-hidden ${size === 6 ? 'w-6 h-6' : size === 7 ? 'w-7 h-7' : size === 8 ? 'w-8 h-8' : size === 9 ? 'w-9 h-9' : size === 10 ? 'w-10 h-10' : size === 11 ? 'w-11 h-11' : size === 12 ? 'w-12 h-12' : size === 14 ? 'w-14 h-14' : size === 16 ? 'w-16 h-16' : size === 20 ? 'w-20 h-20' : 'w-24 h-24'} ${rounded === 'base' ? 'rounded-[32%]' : 'rounded-full'} bg-gray-300`}>
+            <div className={`overflow-hidden ${size === 6 ? 'w-6 h-6' : size === 7 ? 'w-7 h-7' : size === 8 ? 'w-8 h-8' : size === 9 ? 'w-9 h-9' : size === 10 ? 'w-10 h-10' : size === 11 ? 'w-11 h-11' : size === 12 ? 'w-12 h-12' : size === 14 ? 'w-14 h-14' : size === 16 ? 'w-16 h-16' : size === 20 ? 'w-20 h-20' : 'w-24 h-24'} ${variant === 'base' ? 'rounded-[32%]' : 'rounded-full'} bg-gray-300`}>
                 <img src={src} alt={alt ? alt : 'ruskel UI placeholder'} />
             </div>
             <span className='sr-only'>Avatar Icon</span>
--- src/types/Avatar.ts
+++ src/types/Avatar.ts
@@ -1,7 +1,14 @@
-interface RuiAvatarProps {
-    size: '' // size
-    src: '' // string
-    alt: '' // string
-    badgeColor: '' // color [generic]
-    variant: '' // variant
+import { HTMLAttributes } from 'react';
+import { color } from './Generics';
+
+type variant = 'base' | 'rounded'
+
+type size = 6 | 7 | 8 | 9 | 10 | 11 | 12 | 14 | 16 | 20 | 24;
+
+export interface RuiAvatarProps extends HTMLAttributes<HTMLImageElement> {
+    size: size
+    src: string
+    alt: string
+    badgeColor: color
+    variant: variant
 }
