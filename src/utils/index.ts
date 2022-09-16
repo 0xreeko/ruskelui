6830b4beb1eefa38ec2bce746415b573352af05c
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-09-16 09:20:54 +0100

    removed utils to keep types

--- src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export type colorType = 'amber' | 'amethyst' | 'aqua' | 'azure' | 'blue' | 'cyan' | 'emerald' | 'fuchsia' | 'green' | 'indigo' | 'jade' | 'jasper' | 'kunzite' | 'lime' | 'moonstone' | 'orange' | 'peridot' | 'pink' | 'purple' | 'red' | 'rose' | 'ruby' | 'russian' | 'scarlet' | 'stratos' | 'sunstone' | 'sylver' | 'teal' | 'violet' | 'yellow';
-
-export type buttonSize = "small" | "base" | "large" | "xlarge";
-
-export type barFlowType = "r" | "l";
-
-export type gradientFlowType = "tl" | "t" | "tr" | "r" | "br" | "b" | "bl" | "l"
