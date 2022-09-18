3e95c7072c04c0da00dbe081e2be58c8f3cf3e13
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-09-18 12:51:20 +0100

    added active color state to Accordion items

--- src/Tooltip.tsx
+++ src/Tooltip.tsx
@@ -8,7 +8,7 @@ export const RuiTooltip = ({ name, message, children, direction }: RuiTooltipPro
                 <div role={'tooltip'} id={name} className="relative inline-block group">
                     {children}
                     <div className="absolute z-10 max-w-xs px-3 py-1 duration-300 origin-right scale-0 -translate-x-full -translate-y-1/2 rounded-lg opacity-0 top-1/2 -left-3 bg-russian-600 group-hover:scale-100 group-hover:opacity-100 text-sylver-100">
-                        <span>{message}</span>
+                        <span className="text-d-copy">{message}</span>
                     </div>
                 </div>
             );
@@ -17,7 +17,7 @@ export const RuiTooltip = ({ name, message, children, direction }: RuiTooltipPro
                 <div role={'tooltip'} id={name} className="relative inline-block group">
                     <p>{children}</p>
                     <div className="absolute max-w-xs px-3 py-1 duration-300 origin-bottom-right scale-0 -translate-x-full -translate-y-full rounded-lg opacity-0 bg-russian-600 -top-2 -left-3 group-hover:scale-100 group-hover:opacity-100 text-sylver-100">
-                        <span>{message}</span>
+                        <span className="text-d-copy">{message}</span>
                     </div>
                 </div>
             )
@@ -26,7 +26,7 @@ export const RuiTooltip = ({ name, message, children, direction }: RuiTooltipPro
                 <div role={'tooltip'} id={name} className="relative inline-block group">
                     {children}
                     <div className="absolute max-w-lg px-2 py-1 duration-300 origin-bottom transform scale-0 -translate-x-1/2 -translate-y-full rounded-lg opacity-0 bg-russian-600 -top-2 left-1/2 group-hover:scale-100 group-hover:opacity-100 text-sylver-100">
-                        <span>{message}</span>
+                        <span className="text-d-copy">{message}</span>
                     </div>
                 </div>
             )
@@ -35,7 +35,7 @@ export const RuiTooltip = ({ name, message, children, direction }: RuiTooltipPro
                 <div role={'tooltip'} id={name} className="relative inline-block group">
                     <p>{children}</p>
                     <div className="absolute max-w-xs px-3 py-1 duration-300 origin-bottom-left scale-0 translate-x-full -translate-y-full rounded-lg opacity-0 bg-russian-600 -top-2 -right-3 group-hover:scale-100 group-hover:opacity-100 text-sylver-100">
-                        <span>{message}</span>
+                        <span className="text-d-copy">{message}</span>
                     </div>
                 </div>
             )
@@ -44,7 +44,7 @@ export const RuiTooltip = ({ name, message, children, direction }: RuiTooltipPro
                 <div role={'tooltip'} id={name} className="relative inline-block group">
                     {children}
                     <div className="absolute max-w-xs px-3 py-1 duration-300 origin-left scale-0 translate-x-full -translate-y-1/2 rounded-lg opacity-0 bg-russian-600 top-1/2 -right-3 group-hover:scale-100 group-hover:opacity-100 text-sylver-100">
-                        <span>{message}</span>
+                        <span className="text-d-copy">{message}</span>
                     </div>
                 </div>
             )
@@ -53,7 +53,7 @@ export const RuiTooltip = ({ name, message, children, direction }: RuiTooltipPro
                 <div role={'tooltip'} id={name} className="relative inline-block group">
                     {children}
                     <div className="absolute max-w-xs px-3 py-1 duration-300 origin-top-left scale-0 translate-x-full translate-y-1/2 rounded-lg opacity-0 bg-russian-600 top-1/2 -right-3 group-hover:scale-100 group-hover:opacity-100 text-sylver-100">
-                        <span>{message}</span>
+                        <span className="text-d-copy">{message}</span>
                     </div>
                 </div>
             )
@@ -62,7 +62,7 @@ export const RuiTooltip = ({ name, message, children, direction }: RuiTooltipPro
                 <div role={'tooltip'} id={name} className="relative inline-block group">
                     {children}
                     <div className="absolute px-2 py-1 duration-300 origin-top scale-0 -translate-x-1/2 translate-y-full rounded-lg opacity-0 bg-russian-600 -bottom-2 left-1/2 group-hover:scale-100 group-hover:opacity-100 text-sylver-100">
-                        <span>{message}</span>
+                        <span className="text-d-copy">{message}</span>
                     </div>
                 </div>
             )
@@ -71,7 +71,7 @@ export const RuiTooltip = ({ name, message, children, direction }: RuiTooltipPro
                 <div role={'tooltip'} id={name} className="relative inline-block group">
                     {children}
                     <div className="absolute max-w-xs px-3 py-1 duration-300 origin-top-right scale-0 -translate-x-full translate-y-1/2 rounded-lg opacity-0 top-1/2 -left-3 bg-russian-600 group-hover:scale-100 group-hover:opacity-100">
-                        <span>{message}</span>
+                        <span className="text-d-copy">{message}</span>
                     </div>
                 </div>
             )
@@ -80,7 +80,7 @@ export const RuiTooltip = ({ name, message, children, direction }: RuiTooltipPro
                 <div role={'tooltip'} id={name} className="relative inline-block group">
                     {children}
                     <div className="absolute max-w-lg px-2 py-1 duration-300 origin-bottom transform scale-0 -translate-x-1/2 -translate-y-full rounded-lg opacity-0 bg-russian-600 -top-2 left-1/2 group-hover:scale-100 group-hover:opacity-100 text-sylver-100">
-                        <span>{message}</span>
+                        <span className="text-d-copy">{message}</span>
                     </div>
                 </div>
             )
