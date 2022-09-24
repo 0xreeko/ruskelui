bdbaf7c0d5a5d00790633895b8b0224b50b50d1d
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-09-24 17:30:39 +0100

    modded Radio and added css module

--- src/Radio.tsx
+++ src/Radio.tsx
@@ -1,16 +1,14 @@
 import React from "react"
 import { RuiRadioProps } from "./types/Radio"
+// @ts-ignore
+import styles from './modules/Radio.module.css'
 
 export const RuiRadio = ({ label, value, name, variant }: RuiRadioProps) => {
     switch (variant) {
         case "base":
             return (
-                <label className="flex items-center gap-2 group">
-                    <input type="radio" name={name} id={name} value={value} className="sr-only peer" />
-                    <div className="relative flex items-center justify-center w-5 h-5 duration-150 bg-gray-100 border rounded-full appearance-none peer-checked:border-blue-600 peer-checked:bg-blue-400">
-                        <div className="absolute w-2 h-2 delay-150 rounded-full group-focus-within:bg-blue-200 backdrop-blur-sm">
-                        </div>
-                    </div>
+                <label className={`relative inline-block ${styles.radio}`}>
+                    <input type="radio" name={name} id={name} value={value} className="sr-only" />
                     <span className="text-russian-600 dark:text-sylver-100">{label}</span>
                 </label>
             );
