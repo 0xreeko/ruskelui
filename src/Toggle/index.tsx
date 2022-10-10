fd2544b80b81bd5558e9d430995a47f49420ac73
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-10 13:23:27 +0100

    removed repeated base bg color for Toggle and moved it to Toggle codebase, plus left and increased color level when checked

--- src/Toggle/index.tsx
+++ src/Toggle/index.tsx
@@ -6,10 +6,10 @@ import { toggleColor } from './utils/Toggle';
 
 export const RuiToggle = ({color = 'amethyst', name}: RuiToggleProps) => {
     return (
-        <label tabIndex={0} className="relative flex items-center w-10 h-4 overflow-hidden rounded-md cursor-pointer">
+        <label tabIndex={0} className="relative flex items-center w-12 h-6 overflow-hidden rounded-full cursor-pointer backdrop-blur-sm bg-gray-400/80">
             <input role={'switch'} aria-checked="false" aria-label={name ? name : 'toggle placeholder'} type="checkbox" className="sr-only peer" id={name} />
-            <span className={`w-full h-full ${toggleColor[color]} duration-300 transition-all`}></span>
-            <span className={`w-3 h-3 left-1 absolute duration-300 transition-all rounded-[32%] peer-checked:left-6 bg-stratos-100 peer-checked:bg-russian-500`}></span>
+            <span className={`w-full h-full bg-stratos-400/20 ${toggleColor[color]} duration-300 transition-all`}></span>
+            <span className={`w-4 h-4 left-1 absolute duration-300 transition-all rounded-full peer-checked:left-7 active:scale-110 bg-stratos-100`}></span>
         </label>
     )
 }
