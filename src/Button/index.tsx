a0073ffb70e76f02d2100d3aa1df90acbe7a75b7
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-14 02:54:04 +0100

    modded Button

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -5,7 +5,7 @@ export const RuiButton = ({ children, color = 'amethyst', size = "sm", disabled 
   switch (variant) {
     case "ghost":
       return (
-        <button type="button" onClick={onClick} className={`flex ${sizing[size]} w-fit items-center gap-3 rounded-[9px] border-[1.5px] bg-transparent ${ghostButtonStyles[color]} text-sm duration-150 disabled:pointer-events-none disabled:border-gray-300 disabled:text-gray-300 disabled:select-none`} aria-readonly={disabled} disabled={disabled}>
+        <button type="button" onClick={onClick} className={`flex ${sizing[size]} w-fit items-center gap-3 rounded-[9px] border-[1.5px] bg-transparent ${ghostButtonStyles[color]} text-sm duration-150 disabled:pointer-events-none disabled:border-gray-300 disabled:text-gray-300 disabled:select-none`} disabled={disabled}>
           {leftIcon && leftIcon}
           <span>{children}</span>
           {rightIcon && rightIcon}
@@ -13,7 +13,7 @@ export const RuiButton = ({ children, color = 'amethyst', size = "sm", disabled 
       );
     case "text":
       return (
-        <button type="button" onClick={onClick} className={`flex ${sizing[size]} w-fit items-center ${textButtonStyles[color]} gap-3 rounded-[9px] border-[1.5px] border-transparent text-sm backdrop-blur-sm duration-150 disabled:pointer-events-none disabled:text-gray-300 disabled:select-none`} aria-readonly={disabled} disabled={disabled}>
+        <button type="button" onClick={onClick} className={`flex ${sizing[size]} w-fit items-center ${textButtonStyles[color]} gap-3 rounded-[9px] border-[1.5px] border-transparent text-sm backdrop-blur-sm duration-150 disabled:pointer-events-none disabled:text-gray-300 disabled:select-none`} disabled={disabled}>
           {leftIcon && leftIcon}
           <span>{children}</span>
           {rightIcon && rightIcon}
@@ -21,7 +21,7 @@ export const RuiButton = ({ children, color = 'amethyst', size = "sm", disabled 
       );
     case "filled":
       return (
-        <button type="button" onClick={onClick} className={`flex w-fit items-center gap-3 rounded-[9px] border border-transparent ${baseButtonStyles[color]} ${sizing[size]} text-sm text-white duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} aria-readonly={disabled} disabled={disabled}>
+        <button type="button" onClick={onClick} className={`flex w-fit items-center gap-3 rounded-[9px] border-[1.5px] border-transparent ${baseButtonStyles[color]} ${sizing[size]} text-sm text-white duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} disabled={disabled}>
           {leftIcon && leftIcon}
           <span>{children}</span>
           {rightIcon && rightIcon}
@@ -29,13 +29,13 @@ export const RuiButton = ({ children, color = 'amethyst', size = "sm", disabled 
       );
     case "icon":
       return (
-        <button type="button" onClick={onClick} className="flex w-fit px-3 items-center rounded-[9px] bg-sylver-100 dark:bg-russian-800 dark:active:bg-gray-900/80 active:bg-gray-200 py-1.5 duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none" aria-readonly={disabled} disabled={disabled}>
-         {children}
+        <button type="button" onClick={onClick} className="flex w-fit px-3 items-center rounded-[9px] bg-sylver-100 dark:bg-russian-800 dark:active:bg-gray-900/80 active:bg-gray-200 py-1.5 duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none" disabled={disabled}>
+          {children}
         </button>
       )
     default:
       return (
-        <button type="button" onClick={onClick} className={`flex w-fit items-center gap-3 rounded-[9px] border border-transparent ${baseButtonStyles[color]} ${sizing[size]} text-sm text-white duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} aria-readonly={disabled} disabled={disabled}>
+        <button type="button" onClick={onClick} className={`flex w-fit items-center gap-3 rounded-[9px] border border-transparent ${baseButtonStyles[color]} ${sizing[size]} text-sm text-white duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} disabled={disabled}>
           {leftIcon && leftIcon}
           <span>{children}</span>
           {rightIcon && rightIcon}
