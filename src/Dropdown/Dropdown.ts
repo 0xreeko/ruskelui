b59f69da8840596a9da5b59c42a25cdc3ef7c154
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-20 18:29:19 +0100

    removed sylver and moonstone from available colors in Dropdown

--- src/Dropdown/Dropdown.ts
+++ src/Dropdown/Dropdown.ts
@@ -12,6 +12,9 @@ export interface RuiDropdownProps {
 
 export type position = "left" | "top" | "right" | "bottom"
 
+
+type dropdownColor = Extract<color, 'amber' | 'amethyst' | 'aqua' | 'azure' | 'bayoux' | 'blue' | 'cyan' | 'emerald' | 'fuchsia' | 'green' | 'indigo' | 'jade' | 'jasper' | 'kunzite' | 'labrodite' | 'lime' | 'orange' | 'peridot' | 'pink' | 'purple' | 'red' | 'rose' | 'ruby' | 'russian' | 'scarlet' | 'stratos' | 'sunstone' | 'teal' | 'violet' | 'yellow'>
+
 export const positioning: { [key in position]: string } = {
     left: "-top-1.5 -translate-x-full -left-3",
     top: "-translate-y-full",
@@ -19,7 +22,7 @@ export const positioning: { [key in position]: string } = {
     bottom: "mt-6"
 }
 
-export const hoverColor: { [key in color]: string } = {
+export const hoverColor: { [key in dropdownColor]: string } = {
     amber: "hover:bg-amber-400 dark:hover:bg-amber-400/20 dark:hover:text-amber-400",
     amethyst: "hover:bg-amethyst-400 dark:hover:bg-amethyst-400/20 dark:hover:text-amethyst-400",
     aqua: "hover:bg-aqua-400 dark:hover:bg-aqua-400/20 dark:hover:text-aqua-400",
@@ -36,7 +39,6 @@ export const hoverColor: { [key in color]: string } = {
     kunzite: "hover:bg-kunzite-400 dark:hover:bg-kunzite-400/20 dark:hover:text-kunzite-400",
     labrodite: "hover:bg-labrodite-400 dark:hover:bg-labrodite-400/20 dark:hover:text-labrodite-400",
     lime: "hover:bg-lime-400 dark:hover:bg-lime-400/20 dark:hover:text-lime-400",
-    moonstone: "hover:bg-moonstone-400 dark:hover:bg-moonstone-400/20 dark:hover:text-moonstone-400",
     orange: "hover:bg-orange-400 dark:hover:bg-orange-400/20 dark:hover:text-orange-400",
     peridot: "hover:bg-peridot-400 dark:hover:bg-peridot-400/20 dark:hover:text-peridot-400",
     pink: "hover:bg-pink-400 dark:hover:bg-pink-400/20 dark:hover:text-pink-400",
@@ -48,7 +50,6 @@ export const hoverColor: { [key in color]: string } = {
     scarlet: "hover:bg-scarlet-400 dark:hover:bg-scarlet-400/20 dark:hover:text-scarlet-400",
     stratos: "hover:bg-stratos-400 dark:hover:bg-stratos-400/20 dark:hover:text-stratos-400",
     sunstone: "hover:bg-sunstone-400 dark:hover:bg-sunstone-400/20 dark:hover:text-sunstone-400",
-    sylver: "hover:bg-sylver-400 dark:hover:bg-sylver-400/20 dark:hover:text-sylver-400",
     teal: "hover:bg-teal-400 dark:hover:bg-teal-400/20 dark:hover:text-teal-400",
     violet: "hover:bg-violet-400 dark:hover:bg-violet-400/20 dark:hover:text-violet-400",
     yellow: "hover:bg-yellow-400 dark:hover:bg-yellow-400/20 dark:hover:text-yellow-400",
