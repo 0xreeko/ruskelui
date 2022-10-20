7f237e5ff81c30e17073a7c145ced5c86bcb8ede
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-20 21:06:10 +0100

    added id to KBD

--- src/KBD/index.tsx
+++ src/KBD/index.tsx
@@ -3,9 +3,9 @@ import { RuiKBDProps } from './KBD';
 // @ts-ignore
 import styles from './KBD.module.css'
 
-export const RuiKBD = ({children, color = "teal"}: RuiKBDProps) => {
+export const RuiKBD = ({id, children, color = "teal"}: RuiKBDProps) => {
     return (
-        <div className={`${styles.kbd} ${styles[color]}`}>
+        <div id={id} className={`${styles.kbd} ${styles[color]}`}>
             {children}
         </div>
     )
