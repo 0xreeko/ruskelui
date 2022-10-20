0987d1e3d8b06fa03be13237b7396d6352dae9fc
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-20 21:03:47 +0100

    added id to Progress

--- src/Progress/index.tsx
+++ src/Progress/index.tsx
@@ -3,18 +3,18 @@ import { RuiProgressProps, barColor, donutColor, positionVar } from './Progress'
 // @ts-ignore
 import styles from './Progress.module.css'
 
-export const RuiProgress = ({ name, percentage, isGradient, color = 'amethyst', startColor = 'blue', endColor = 'amethyst', barDirection = 'right', variant = 'bar' }: RuiProgressProps) => {
+export const RuiProgress = ({ id, percentage, isGradient, color = 'amethyst', startColor = 'blue', endColor = 'amethyst', barDirection = 'right', variant = 'bar' }: RuiProgressProps) => {
     switch (variant) {
         case "bar":
             return (
-                <div id={name} tabIndex={0} role={'progressbar'} className={styles.progress}>
+                <div id={id} tabIndex={0} role={'progressbar'} className={styles.progress}>
                     <div className={`h-full absolute rounded-full duration-300 ${isGradient ? `${positionVar[barDirection]} ${barColor[startColor][1]} ${barColor[endColor][2]}` : `${barColor[color][0]}`}`} style={{ width: `${percentage}%` }}></div>
                     <span className="sr-only">Progress Bar</span>
                 </div>
             );
         case "donut":
             return (
-                <div id={name} className="relative inline-flex items-center justify-center">
+                <div id={id} className="relative inline-flex items-center justify-center">
                     <div className={`absolute text-d-h4 font-semibold ${donutColor[color]}`}>{percentage}%</div>
                     <svg className={styles.svg} viewBox="0 0 120 120" >
                         <circle className="text-black/30" strokeWidth={4} stroke="currentColor" fill="transparent" r="54" cx="60" cy="60" />
@@ -26,7 +26,7 @@ export const RuiProgress = ({ name, percentage, isGradient, color = 'amethyst', 
 
         default:
             return (
-                <div id={name} tabIndex={0} role={'progressbar'} className={styles.progress}>
+                <div id={id} tabIndex={0} role={'progressbar'} className={styles.progress}>
                     <div className={`h-full absolute rounded-full duration-300 ${isGradient ? `${positionVar[barDirection]} ${barColor[startColor][1]} ${barColor[endColor][2]}` : `${barColor[color][0]}`}`} style={{ width: `${percentage}%` }}></div>
                     <span className="sr-only">Progress Bar</span>
                 </div>
