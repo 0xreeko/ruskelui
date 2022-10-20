9b2e000378033a6edc6f8040c1815d64010da46e
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-20 20:37:02 +0100

    modded Radio with new props

--- src/Radio/index.tsx
+++ src/Radio/index.tsx
@@ -2,26 +2,26 @@ import React from "react"
 import { RuiRadioProps } from "./Radio"
 import { radioColor } from "./utils/color";
 
-export const RuiRadio = ({ value, name, variant, color = "teal", disabled }: RuiRadioProps) => {
+export const RuiRadio = ({id, defaultChecked, defaultValue, value, name, variant, color = "teal", disabled }: RuiRadioProps) => {
     switch (variant) {
         case "base":
             return (
                 <div className={`inline-flex relative items-center justify-center`}>
-                    <input type="radio" name={name} id={name} value={value} className={`relative w-5 h-5 border-2 rounded-full appearance-none peer ${disabled !== true ? `${radioColor[color][1]}` : "border-sylver-800 pointer-events-none"}`} />
+                    <input type="radio" defaultValue={defaultValue} defaultChecked={defaultChecked} name={name} id={id} value={value} className={`relative w-5 h-5 border-2 rounded-full appearance-none peer ${disabled !== true ? `${radioColor[color][1]}` : "border-sylver-800 pointer-events-none"}`} />
                     <div className={`absolute w-2.5 h-2.5 ${radioColor[color][0]} rounded-full scale-0 duration-300 peer-checked:scale-100`}></div>
                 </div>
             );
         case "bordered":
             return (
-                <input type="radio" name={name} id={name} value={value} className={`appearance-none peer border-2 w-5 h-5 rounded-full checked:border-[6px] hover:border-opacity-80 ${disabled !== true ? `${radioColor[color][1]}` : "border-sylver-800 pointer-events-none"} duration-300`}></input>
+                <input type="radio" name={name} id={id} value={value} className={`appearance-none peer border-2 w-5 h-5 rounded-full checked:border-[6px] hover:border-opacity-80 ${disabled !== true ? `${radioColor[color][1]}` : "border-sylver-800 pointer-events-none"} duration-300`}></input>
             )
         case "ghost":
             return (
-                <input type="radio" name={name} id={name} value={value} className="sr-only peer" />
+                <input type="radio" name={name} id={id} value={value} className="sr-only peer" />
             );
         default:
             return (
-                <input type="radio" name={name} id={name} value={value} className="sr-only peer" />
+                <input type="radio" name={name} id={id} value={value} className="sr-only peer" />
             )
     }
 }
