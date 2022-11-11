b1d3411e11be348c60f0a55e6f600109a7ff56e8
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-11 15:28:02 +0000

    renamed padding token

--- src/Pill/index.tsx
+++ src/Pill/index.tsx
@@ -4,7 +4,7 @@ import { RuiPillProps, colorVar } from './Pill'
 export const RuiPill = ({id, children, color = "ruby", svg}: RuiPillProps) => {
 
     return (
-        <div id={id} className={`${colorVar[color]} flex items-center duration-300 select-none w-fit px-2 py-1 mobile-overline rounded-[6px]`}>
+        <div id={id} className={`${colorVar[color]} flex items-center duration-300 select-none w-fit rui-padding mobile-overline rounded-[6px]`}>
             <slot name='svg'>{svg}</slot>
             <span>{children}</span>
         </div>
