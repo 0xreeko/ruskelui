7edf5046701c6f5f3244bf8d7bddd5bb75735cdd
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-11 15:21:55 +0000

    global padding for all elements

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -6,7 +6,7 @@ export const RuiButton = ({ id, form, isLoading, loadingLabel, fullWidth, value,
   switch (variant) {
     case "ghost":
       return (
-        <button id={id} form={form} value={value} type={type} onClick={onClick} data-ripple={ripple === true} className={`flex active:scale-95 ${sizing[size]} ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] bg-transparent ${ghostButtonStyles[color]} text-sm duration-150 disabled:pointer-events-none disabled:border-gray-300 disabled:text-gray-300 disabled:select-none`} disabled={disabled}>
+        <button id={id} form={form} value={value} type={type} onClick={onClick} data-ripple={ripple === true} className={`flex active:scale-95 rui-pXpY ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] bg-transparent ${ghostButtonStyles[color]} text-sm duration-150 disabled:pointer-events-none disabled:border-gray-300 disabled:text-gray-300 disabled:select-none`} disabled={disabled}>
           {isLoading ? (
             <>
               {loadingSpinner}
@@ -24,7 +24,7 @@ export const RuiButton = ({ id, form, isLoading, loadingLabel, fullWidth, value,
       );
     case "text":
       return (
-        <button id={id} form={form} value={value} type={type} onClick={onClick} data-ripple={ripple === true} className={`flex active:scale-95 ${sizing[size]} ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center ${textButtonStyles[color]} gap-3 rounded-[9px] border-[1.5px] border-transparent text-sm backdrop-blur-sm duration-150 disabled:pointer-events-none disabled:text-gray-300 disabled:select-none`} disabled={disabled}>
+        <button id={id} form={form} value={value} type={type} onClick={onClick} data-ripple={ripple === true} className={`flex active:scale-95 rui-pXpY ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center ${textButtonStyles[color]} gap-3 rounded-[9px] border-[1.5px] border-transparent text-sm backdrop-blur-sm duration-150 disabled:pointer-events-none disabled:text-gray-300 disabled:select-none`} disabled={disabled}>
           {isLoading ? (
             <>
               {loadingSpinner}
@@ -42,7 +42,7 @@ export const RuiButton = ({ id, form, isLoading, loadingLabel, fullWidth, value,
       );
     case "filled":
       return (
-        <button id={id} form={form} value={value} type={type} onClick={onClick} data-ripple={ripple === true} className={`flex active:scale-95 ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] border-transparent ${filledButtonStyles[color]} ${sizing[size]} text-sm text-white duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} disabled={disabled}>
+        <button id={id} form={form} value={value} type={type} onClick={onClick} data-ripple={ripple === true} className={`flex active:scale-95 ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] border-transparent ${filledButtonStyles[color]} rui-pXpY text-sm text-white duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} disabled={disabled}>
           {isLoading ? (
             <>
               {loadingSpinner}
@@ -60,13 +60,13 @@ export const RuiButton = ({ id, form, isLoading, loadingLabel, fullWidth, value,
       );
     case "icon":
       return (
-        <button id={id} form={form} value={value} type={type} onClick={onClick} data-ripple={ripple === true} className={`flex active:scale-95 ${fullWidth ? 'w-full' : 'w-fit'} px-3 items-center justify-center rounded-[9px] bg-sylver-100 dark:bg-russian-800 dark:active:bg-gray-900/80 active:bg-gray-200 py-1.5 duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} disabled={disabled}>
+        <button id={id} form={form} value={value} type={type} onClick={onClick} data-ripple={ripple === true} className={`flex active:scale-95 ${fullWidth ? 'w-full' : 'w-fit'} rui-pXpY items-center justify-center rounded-[9px] bg-sylver-100 dark:bg-russian-800 dark:active:bg-gray-900/80 active:bg-gray-200 duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} disabled={disabled}>
           {children}
         </button>
       )
     default:
       return (
-        <button id={id} form={form} value={value} type={type} onClick={onClick} data-ripple={ripple === true} className={`flex active:scale-95 ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] border-transparent ${filledButtonStyles[color]} ${sizing[size]} text-sm text-white duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} disabled={disabled}>
+        <button id={id} form={form} value={value} type={type} onClick={onClick} data-ripple={ripple === true} className={`flex active:scale-95 ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] border-transparent ${filledButtonStyles[color]} rui-pXpY text-sm text-white duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} disabled={disabled}>
           {isLoading ? (
             <>
               {loadingSpinner}
