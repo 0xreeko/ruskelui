19d7430cce6b72022bb7e9cdee6216c0aba27ab8
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-12 18:42:31 +0000

    updated breadcrumbs

--- src/Breadcrumbs/Breadcrumbs.ts
+++ src/Breadcrumbs/Breadcrumbs.ts
@@ -2,9 +2,10 @@ import { ReactNode } from "react"
 import { color } from "../types/Generics"
 
 type variant = "current" | "previous"
+type breadCrumbsColor = Extract<color, 'amethyst' | 'lazure' | 'citrine' | 'jade' | 'purpalite' | 'rosian' | 'scarlet' | 'sunstone' | 'tingual' >
 
 export interface RuiBreadcrumbsProps {
-    color?: color
+    color?: breadCrumbsColor
     route?: string
     fullWidth?: boolean
     divider?: ReactNode
