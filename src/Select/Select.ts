e83259f806100d3202852a0418a9cbc02775f5aa
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-13 13:10:26 +0000

    .

--- src/Select/Select.ts
+++ src/Select/Select.ts
@@ -7,6 +7,7 @@ export interface RuiSelectProps {
     label: string
     children: ReactNode
     selectedIcon: ReactNode
+    markerColor: selectColorType
 }
 
 export interface RuiSelectChildProps {
@@ -15,7 +16,7 @@ export interface RuiSelectChildProps {
     disabled?: boolean
 }
 
-export const selectColor: { [key in selectColorType]: string } = {
+export const selectItemColor: { [key in selectColorType]: string } = {
     amethyst: "hover:bg-amethyst-400",
     bayoux: "hover:bg-bayoux-400",
     citrine: "hover:bg-citrine-400",
@@ -26,4 +27,17 @@ export const selectColor: { [key in selectColorType]: string } = {
     scarlet: "hover:bg-scarlet-400",
     sunstone: "hover:bg-sunstone-400",
     tingual: "hover:bg-tingual-400",
+}
+
+export const selectMarkerColor: { [key in selectColorType]: string } = {
+    amethyst: "text-amethyst-400",
+    bayoux: "text-bayoux-400",
+    citrine: "text-citrine-400",
+    lazure: "text-lazure-400",
+    jade: "text-jade-400",
+    purpalite: "text-purpalite-400",
+    rosian: "text-rosian-400",
+    scarlet: "text-scarlet-400",
+    sunstone: "text-sunstone-400",
+    tingual: "text-tingual-400",
 }
--- src/Select/index.tsx
+++ src/Select/index.tsx
@@ -1,19 +1,19 @@
 import React, { useContext, useState } from 'react'
 import { SelectContext } from './SelectContext'
-import { RuiSelectChildProps, RuiSelectProps, selectColor} from './Select'
+import { RuiSelectChildProps, RuiSelectProps, selectItemColor, selectMarkerColor} from './Select'
 
 export const SelectItem = ({ value, color = "amethyst", disabled }: RuiSelectChildProps) => {
     const { handleChange, selectedIcon, currentValue } = useContext(SelectContext)
 
     return (
-        <li tabIndex={0} className={`flex justify-between duration-150 hover:text-onyx text-neropside dark:text-onyx dark:hover:text-neropside hover:cursor-pointer py-2 px-4 ${disabled !== true  ? selectColor[color] : "text-neropside/40 dark:text-onyx/40 pointer-events-none select-none"}`} onClick={() => handleChange?.(value)}>
+        <li tabIndex={0} className={`flex justify-between duration-150 hover:text-onyx text-neropside dark:text-onyx dark:hover:text-neropside hover:cursor-pointer py-2 px-4 ${disabled !== true  ? selectItemColor[color] : "text-neropside/40 dark:text-onyx/40 pointer-events-none select-none"}`} onClick={() => handleChange?.(value)}>
             <span>{value}</span>
             <span>{currentValue === value ? selectedIcon : ""}</span>
         </li>
     )
 }
 
-export const RuiSelect = ({ label, children, selectedIcon = "🔥" }: RuiSelectProps) => {
+export const RuiSelect = ({ label, children, selectedIcon = "🔥", markerColor }: RuiSelectProps) => {
     const [currentValue, setCurrentValue] = useState('');
     const [open, setOpen] = useState(false);
 
@@ -34,10 +34,10 @@ export const RuiSelect = ({ label, children, selectedIcon = "🔥" }: RuiSelectP
         <SelectContext.Provider value={{ label, currentValue, handleChange, selectedIcon }}>
             <div className="relative w-72 group">
                 <span className='sr-only'>Select Dropdown</span>
-                <button className={`peer flex w-full items-center justify-between border border-obsidian-300 dark:border-obsidian-800 rounded-lg bg-obsidian-200 dark:bg-obsidian-400/40 py-1 px-4`} onClick={() => onChange()}>
+                <button className={`peer flex w-full items-center justify-between border border-obsidian-300 dark:border-obsidian-800 rounded-lg bg-obsidian-200 dark:bg-obsidian-400/40 py-2 px-4`} onClick={() => onChange()}>
                     <span className="whitespace-nowrap text-neropside dark:text-onyx">{currentValue !== "" ? currentValue : label}</span>
                     <div className="pl-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" className={`${open === true ? "group-focus-within:rotate-180" : "rotate-0"} duration-300 text-scarlet-400`} width="16" height="16" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                        <svg xmlns="http://www.w3.org/2000/svg" className={`${open === true ? "group-focus-within:rotate-180" : "rotate-0"} duration-300 ${selectMarkerColor[markerColor]}`} width="16" height="16" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <polyline points="6 9 12 15 18 9"></polyline>
                         </svg>
