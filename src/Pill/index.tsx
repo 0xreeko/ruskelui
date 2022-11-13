1e1d98e96b5d005dc14313b784b9868b58759f50
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-13 13:51:35 +0000

    .

--- src/Pill/index.tsx
+++ src/Pill/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { RuiPillProps, colorVar } from './Pill'
 
-export const RuiPill = ({id, children, color = "ruby", svg}: RuiPillProps) => {
+export const RuiPill = ({id, children, color = "rosian", svg}: RuiPillProps) => {
 
     return (
-        <div id={id} className={`${colorVar[color]} flex items-center duration-300 select-none w-fit rui-padding mobile-overline rounded-[6px]`}>
+        <div id={id} className={`${colorVar[color]} flex items-center duration-300 select-none w-fit py-1.5 px-3 mobile-overline rounded-[9px]`}>
             <slot name='svg'>{svg}</slot>
             <span>{children}</span>
         </div>
