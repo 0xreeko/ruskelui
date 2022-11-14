436f94a062d810845bb1fbdb04b7d8bfe3939f91
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-14 11:36:29 +0000

    number input patterned

--- src/Input/InputIcons.tsx
+++ src/Input/InputIcons.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export const visibleEye = 
-    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye" width={16} height={16} viewBox="0 0 24 24" stroke-width={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <svg xmlns="http://www.w3.org/2000/svg" className="rui-eyeOn" width={16} height={16} viewBox="0 0 24 24" stroke-width={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
         <circle cx={12} cy={12} r={2}></circle>
         <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path>
     </svg>
 
 export const hiddenEye = 
-    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-eye-off" width={16} height={16} viewBox="0 0 24 24" stroke-width={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+    <svg xmlns="http://www.w3.org/2000/svg" className="rui-eyeOff" width={16} height={16} viewBox="0 0 24 24" stroke-width={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
         <line x1={3} y1={3} x2={21} y2={21}></line>
         <path d="M10.584 10.587a2 2 0 0 0 2.828 2.83"></path>
