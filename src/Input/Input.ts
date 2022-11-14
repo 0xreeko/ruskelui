2a73a97a1866c7ff7acfa297a5f8217084527710
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-14 00:07:58 +0000

    .

--- src/Input/Input.ts
+++ src/Input/Input.ts
@@ -1,7 +1,8 @@
 import { InputHTMLAttributes, ReactNode } from "react"
 import { color } from "../types/Generics"
 
-type variant = "text" | "password" | "email" |"number" | "time"
+type variant = "text" | "password" | "email" | "number" | "time" | "date"
+type position = 'left' | 'right'
 
 export interface RuiInputProps extends InputHTMLAttributes<HTMLInputElement>{
     label: string
@@ -9,43 +10,24 @@ export interface RuiInputProps extends InputHTMLAttributes<HTMLInputElement>{
     fullWidth: boolean
     isDark: boolean
     color: color
-    leftIcon: ReactNode
-    rightIcon: ReactNode
+    icon: ReactNode
+    iconPosition?: position
     name: string
     variant: variant
 }
 
 export const inputColor: { [key in color]: string[] } = {
-    amber: ["group-focus-within:ring-amber-400 dark:focus:ring-amber-400", "group-focus-within:text-amber-400"],
     amethyst: ["group-focus-within:ring-amethyst-400 dark:focus:ring-amethyst-400", "group-:text-amethyst-400"],
-    aqua: ["group-focus-within:ring-aqua-400 dark:focus:ring-aqua-400", "group-focus-within:text-aqua-400"],
-    azure: ["group-focus-within:ring-azure-400 dark:focus:ring-azure-400", "group-focus-within:text-azure-400"],
+    lazure: ["group-focus-within:ring-lazure-400 dark:focus:ring-lazure-400", "group-focus-within:text-lazure-400"],
     bayoux: ["group-focus-within:ring-bayoux-400 dark:focus:ring-bayoux-400", "group-focus-within:text-bayoux-400"],
-    blue: ["group-focus-within:ring-blue-400 dark:focus:ring-blue-400", "group-focus-within:text-blue-400"],
-    cyan: ["group-focus-within:ring-cyan-400 dark:focus:ring-cyan-400", "group-focus-within:text-cyan-400"],
-    emerald: ["group-focus-within:ring-emerald-400 dark:focus:ring-emerald-400", "group-focus-within:text-emerald-400"],
-    fuchsia: ["group-focus-within:ring-fuchsia-400 dark:focus:ring-fuchsia-400", "group-focus-within:text-fuchsia-400"],
-    green: ["group-focus-within:ring-green-400 dark:focus:ring-green-400", "group-focus-within:text-green-400"],
-    indigo: ["group-focus-within:ring-indigo-400 dark:focus:ring-indigo-400", "group-focus-within:text-indigo-400"],
     jade: ["group-focus-within:ring-jade-400 dark:focus:ring-jade-400", "group-focus-within:text-jade-400"],
-    jasper: ["group-focus-within:ring-jasper-400 dark:focus:ring-jasper-400", "group-focus-within:text-jasper-400"],
-    kunzite: ["group-focus-within:ring-kunzite-400 dark:focus:ring-kunzite-400", "group-focus-within:text-kunzite-400"],
-    labrodite: ["group-focus-within:ring-labrodite-400 dark:focus:ring-labrodite-400", "group-focus-within:text-labrodite-400"],
-    lime: ["group-focus-within:ring-lime-400 dark:focus:ring-lime-400", "group-focus-within:text-lime-400"],
-    moonstone: ["group-focus-within:ring-moonstone-400 dark:focus:ring-moonstone-400", "group-focus-within:text-moonstone-400"],
-    orange: ["group-focus-within:ring-orange-400 dark:focus:ring-orange-400", "group-focus-within:text-orange-400"],
-    peridot: ["group-focus-within:ring-peridot-400 dark:focus:ring-peridot-400", "group-focus-within:text-peridot-400"],
-    pink: ["group-focus-within:ring-pink-400 dark:focus:ring-pink-400", "group-focus-within:text-pink-400"],
-    purple: ["group-focus-within:ring-purple-400 dark:focus:ring-purple-400", "group-focus-within:text-purple-400"],
-    red: ["group-focus-within:ring-red-400 dark:focus:ring-red-400", "group-focus-within:text-red-400"],
-    rose: ["group-focus-within:ring-rose-400 dark:focus:ring-rose-400", "group-focus-within:text-rose-400"],
-    ruby: ["group-focus-within:ring-ruby-400 dark:focus:ring-ruby-400", "group-focus-within:text-ruby-400"],
-    russian: ["group-focus-within:ring-russian-400 dark:focus:ring-russian-400", "group-focus-within:text-russian-400"],
+    purpalite: ["group-focus-within:ring-purpalite-400 dark:focus:ring-purpalite-400", "group-focus-within:text-purpalite-400"],
+    rosian: ["group-focus-within:ring-rosian-400 dark:focus:ring-rosian-400", "group-focus-within:text-rosian-400"],
     scarlet: ["group-focus-within:ring-scarlet-400 dark:focus:ring-scarlet-400", "group-focus-within:text-scarlet-400"],
-    stratos: ["group-focus-within:ring-stratos-400 dark:focus:ring-stratos-400", "group-focus-within:text-stratos-400"],
+    obsidian: ["group-focus-within:ring-obsidian-400 dark:focus:ring-obsidian-400", "group-focus-within:text-obsidian-400"],
     sunstone: ["group-focus-within:ring-sunstone-400 dark:focus:ring-sunstone-400", "group-focus-within:text-sunstone-400"],
-    sylver: ["group-focus-within:ring-sylver-400 dark:focus:ring-sylver-400", "group-focus-within:text-sylver-400"],
-    teal: ["group-focus-within:ring-teal-400 dark:focus:ring-teal-400", "group-focus-within:text-teal-400"],
-    violet: ["group-focus-within:ring-violet-400 dark:focus:ring-violet-400", "group-focus-within:text-violet-400"],
-    yellow: ["group-focus-within:ring-yellow-400 dark:focus:ring-yellow-400", "group-focus-within:text-yellow-400"]
+    tingual: ["group-focus-within:ring-tingual-400 dark:focus:ring-tingual-400", "group-focus-within:text-tingual-400"],
+    citrine: ["group-focus-within:ring-citrine-400 dark:focus:ring-citrine-400", "group-focus-within:text-citrine-400"],
+    neropside:  ["group-focus-within:ring-neropside/60 dark:focus:ring-neropside/60", "group-focus-within:text-neropside/60"],
+    onyx: ["group-focus-within:ring-onyx/60 dark:focus:ring-onyx/60", "group-focus-within:text-onyx/60"],
 }
--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -4,21 +4,40 @@ import { hiddenEye, visibleEye } from "./InputIcons";
 // @ts-ignore
 import styles from './Input.module.css'
 
-export const RuiInput = ({ id, disabled, isDark, label, placeholder, pattern, max, min, variant, value, name, color = "kunzite", fullWidth = false, leftIcon, rightIcon, ...props }: RuiInputProps) => {
+export const RuiInput = ({ id, disabled, isDark, label, placeholder, pattern, max, min, variant, value, name, color = "amethyst", fullWidth = false, icon, iconPosition = 'right', ...props }: RuiInputProps) => {
     const [visible, setVisible] = useState(false)
     switch (variant) {
         case "text":
             return (
-                <div id={name} className={`relative flex items-center justify-between text-russian-600 dark:text-sylver-200 ${fullWidth ? "w-full" : "w-full max-w-xs"} ${disabled ? 'disabled:pointer-events-none' : ''}`} >
-                    <input id={id} type="text" value={value} className={`peer py-1 ${leftIcon ? "pl-9" : "pl-2"} ${rightIcon ? "pr-9" : "pr-2"} w-full transition-all duration-300 outline-none bg-white/20 dark:bg-black/20 ring-[1px] backdrop-blur-sm ring-sylver-600 dark:ring-sylver-800 ${inputColor[color][0]} ${placeholder ? "" : "placeholder-transparent"} rounded-lg ${disabled ? 'disabled:pointer-events-none' : ''}`} placeholder={`${placeholder ? placeholder : "estoesunejemplo@es.com"}`} disabled={disabled} />
-                    <span className={`absolute inline-flex left-0 ml-2 pointer-events-none transition duration-300 scale-75 peer-focus:scale-100 ${color ? inputColor[color][1] : 'peer-focus:text-[#6387f1]'}`}>{leftIcon}</span>
-                    <span className={`absolute inline-flex right-0 mr-2 pointer-events-none transition duration-300 scale-75 ${color ? inputColor[color][1] : 'peer-focus:text-[#6387f1]'}`}>{rightIcon}</span>
+                // <div id={name} className={`relative ${fullWidth ? "w-full" : "w-full max-w-xs"}`} >
+                //     {/* <input id={id} type="text" value={value} className={`peer py-2  px-4 ${leftIcon ? "pl-10" : ""} ${rightIcon ? "pr-10" : ""} w-full transition-all duration-300 outline-none bg-white/20 dark:bg-black/20 ring-[1px] backdrop-blur-sm ring-sylver-600 dark:ring-sylver-800 ${inputColor[color][0]} ${placeholder ? "" : "placeholder-transparent"} rounded-lg ${disabled ? 'disabled:pointer-events-none' : ''}`} placeholder={`${placeholder ? placeholder : "estoesunejemplo@es.com"}`} disabled={disabled} /> */}
+                //     <input type="email" id="UserEmail" placeholder="flea@rhcp.com" autoComplete="off" className={`peer w-full rounded-md border border-obsidian-200 py-2 px-4 ${leftIcon ? "pl-10" : ""} ${rightIcon ? "pr-10" : ""} ${disabled ? 'disabled:pointer-events-none' : ''} ${inputColor[color][0]} backdrop-blur-sm bg-onyx/20 dark:bg-neropside/20 shadow-sm outline-none duration-200 placeholder:text-obsidian-500 group-hover:border-jade-500 sm:text-sm`} disabled={disabled} />
+                //     <span className="absolute inset-y-0 right-0 grid w-0 duration-200 pointer-events-none place-content-center text-obsidian-500 group-hover:text-jade-500">
+                //         <svg xmlns="http://www.w3.org/2000/svg" className="" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                //             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                //             <circle cx="12" cy="12" r="4"></circle>
+                //             <path d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28"></path>
+                //         </svg>
+                //     </span>
+                //     <span className={`absolute inline-flex left-0 ml-2 pointer-events-none transition duration-300 scale-75  ${color ? inputColor[color][1] : 'peer-focus:text-[#6387f1]'}`}>{leftIcon}</span>
+                //     <span className={`absolute inline-flex right-0 mr-2 pointer-events-none transition duration-300 scale-75 ${color ? inputColor[color][1] : 'peer-focus:text-[#6387f1]'}`}>{rightIcon}</span>
+                //     <span className="sr-only">Input</span>
+                // </div>
+                <div className={`relative ${fullWidth ? "w-full" : "w-full max-w-xs"}`} >
+                    {/* <input id={id} type="text" value={value} className={`peer py-2  px-4 ${leftIcon ? "pl-10" : ""} ${rightIcon ? "pr-10" : ""} w-full transition-all duration-300 outline-none bg-white/20 dark:bg-black/20 ring-[1px] backdrop-blur-sm ring-sylver-600 dark:ring-sylver-800 ${inputColor[color][0]} ${placeholder ? "" : "placeholder-transparent"} rounded-lg ${disabled ? 'disabled:pointer-events-none' : ''}`} placeholder={`${placeholder ? placeholder : "estoesunejemplo@es.com"}`} disabled={disabled} /> */}
+                    <span className="sr-only">Input</span>
+                    <input type="text" id={id} placeholder="example@rhcp.com" autoComplete="off" className={`peer w-full rounded-md border border-obsidian-300 dark:border-obsidian-800 hover:border-jade-500 text-neropside dark:text-onyx py-2 px-4 ${iconPosition === "left" ? "pl-10" : "pr-10"} ${disabled ? 'disabled:pointer-events-none' : ''} ${inputColor[color][0]} backdrop-blur-sm bg-onyx/40 dark:bg-neropside/40 shadow-sm outline-none duration-200 placeholder:text-obsidian-400 group-hover:border-jade-500 sm:text-sm`} disabled={disabled} />
+                    <span className={`absolute inset-y-0 ${iconPosition === 'left' ? 'left-0' : 'right-0'} grid w-0 duration-200 pointer-events-none place-content-center text-obsidian-500 group-hover:text-jade-500`}>
+                        {icon && (
+                            icon
+                        )}
+                    </span>
                     <span className="sr-only">Input</span>
                 </div>
             )
         case 'password':
             return (
-                <div id={name} className={`relative group flex items-center justify-between text-russian-600 dark:text-sylver-200 ${fullWidth ? "w-full" : "w-full max-w-xs"} ${disabled ? 'disabled:pointer-events-none' : ''}`} >
+                <div className={`relative group flex items-center justify-between text-russian-600 dark:text-sylver-200 ${fullWidth ? "w-full" : "w-full max-w-xs"} ${disabled ? 'disabled:pointer-events-none' : ''}`} >
                     <input id={id} type={visible ? 'text' : 'password'} className={`box-border py-1 pl-2 pr-9 w-full transition-all duration-300 outline-none bg-white/20 dark:bg-black/20 ring-[1px] backdrop-blur-sm ring-sylver-600 dark:ring-sylver-800 ${inputColor[color][0]} ${placeholder ? "" : "placeholder-transparent"} rounded-lg`} placeholder={`${placeholder ? placeholder : "estoesunejemplo@es.com"} ${disabled ? 'disabled:pointer-events-none' : ''}`} disabled={disabled} />
                     <button onClick={() => setVisible(prev => (prev = !prev))} className={`absolute inline-flex right-0 mr-2 scale-75 ${inputColor[color][1]}`}>{!visible ? hiddenEye : visibleEye}</button>
                     <span className="sr-only">Input: Password</span>
@@ -26,9 +45,17 @@ export const RuiInput = ({ id, disabled, isDark, label, placeholder, pattern, ma
             )
         case 'email':
             return (
-                <div id={name} className={`relative group flex items-center justify-between text-russian-600 dark:text-sylver-200 ${fullWidth ? "w-full" : "w-full max-w-xs"} ${disabled ? 'disabled:pointer-events-none' : ''}`} >
-                    <input id={id} type={'email'} pattern={pattern} className={`box-border py-1 pl-2 pr-9 w-full transition-all duration-300 outline-none bg-white/20 dark:bg-black/20 ring-[1px] backdrop-blur-sm ring-sylver-600 dark:ring-sylver-800 ${inputColor[color][0]} ${placeholder ? "" : "placeholder-transparent"} rounded-lg`} placeholder={`${placeholder ? placeholder : "estoesunejemplo@es.com"} ${disabled ? 'disabled:pointer-events-none' : ''}`} disabled={disabled} />
-                    <span className="sr-only">Input: Email</span>
+                <div className={`relative ${fullWidth ? "w-full" : "w-full max-w-xs"}`} >
+                    {/* <input id={id} type="text" value={value} className={`peer py-2  px-4 ${leftIcon ? "pl-10" : ""} ${rightIcon ? "pr-10" : ""} w-full transition-all duration-300 outline-none bg-white/20 dark:bg-black/20 ring-[1px] backdrop-blur-sm ring-sylver-600 dark:ring-sylver-800 ${inputColor[color][0]} ${placeholder ? "" : "placeholder-transparent"} rounded-lg ${disabled ? 'disabled:pointer-events-none' : ''}`} placeholder={`${placeholder ? placeholder : "estoesunejemplo@es.com"}`} disabled={disabled} /> */}
+                    <input type="email" id={id} placeholder="example@rhcp.com" autoComplete="off" className={`peer w-full rounded-md border border-obsidian-300 dark:border-obsidian-800 hover:border-jade-500 text-neropside dark:text-onyx py-2 px-4 ${iconPosition === "left" ? "pl-10" : "pr-10"} ${disabled ? 'disabled:pointer-events-none' : ''} ${inputColor[color][0]} backdrop-blur-sm bg-onyx/40 dark:bg-neropside/40 shadow-sm outline-none duration-200 placeholder:text-obsidian-400 sm:text-sm`} disabled={disabled} />
+                    <span className="absolute inset-y-0 right-0 grid w-10 duration-200 pointer-events-none place-content-center peer-placeholder-shown:text-obsidian-400 text-jade-500">
+                        <svg xmlns="http:ww.w3.org/2000/svg" className="" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="12" cy="12" r="4"></circle>
+                            <path d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28"></path>
+                        </svg>
+                    </span>
+                    <span className="sr-only">Input</span>
                 </div>
             )
         case 'number':
@@ -41,10 +68,16 @@ export const RuiInput = ({ id, disabled, isDark, label, placeholder, pattern, ma
             )
         default:
             return (
-                <div id={name} className={`relative flex items-center justify-between text-russian-600 dark:text-sylver-200 ${fullWidth ? "w-full" : "w-full max-w-xs"} ${disabled ? 'disabled:pointer-events-none' : ''}`} >
-                    <input id={id} type="text" value={value} className={`peer py-1 ${leftIcon ? "pl-9" : "pl-2"} ${rightIcon ? "pr-9" : "pr-2"} w-full transition-all duration-300 outline-none bg-white/20 dark:bg-black/20 ring-[1px] backdrop-blur-sm ring-sylver-600 dark:ring-sylver-800 ${inputColor[color][0]} ${placeholder ? "" : "placeholder-transparent"} rounded-lg ${disabled ? 'disabled:pointer-events-none' : ''}`} placeholder={`${placeholder ? placeholder : "estoesunejemplo@es.com"}`} disabled={disabled} />
-                    <span className={`absolute inline-flex left-0 ml-2 pointer-events-none transition duration-300 scale-75 peer-focus:scale-100 ${color ? inputColor[color][1] : 'peer-focus:text-[#6387f1]'}`}>{leftIcon}</span>
-                    <span className={`absolute inline-flex right-0 mr-2 pointer-events-none transition duration-300 scale-75 ${color ? inputColor[color][1] : 'peer-focus:text-[#6387f1]'}`}>{rightIcon}</span>
+                <div className={`relative ${fullWidth ? "w-full" : "w-full max-w-xs"}`} >
+                    {/* <input id={id} type="text" value={value} className={`peer py-2  px-4 ${leftIcon ? "pl-10" : ""} ${rightIcon ? "pr-10" : ""} w-full transition-all duration-300 outline-none bg-white/20 dark:bg-black/20 ring-[1px] backdrop-blur-sm ring-sylver-600 dark:ring-sylver-800 ${inputColor[color][0]} ${placeholder ? "" : "placeholder-transparent"} rounded-lg ${disabled ? 'disabled:pointer-events-none' : ''}`} placeholder={`${placeholder ? placeholder : "estoesunejemplo@es.com"}`} disabled={disabled} /> */}
+                    <input type="email" id="UserEmail" placeholder="example@rhcp.com" autoComplete="off" className={`peer w-full rounded-md border border-obsidian-300 dark:border-obsidian-800  py-2 px-4 ${iconPosition === "left" ? "pl-10" : "pr-10"} ${disabled ? 'disabled:pointer-events-none' : ''} ${inputColor[color][0]} backdrop-blur-sm bg-onyx/40 dark:bg-neropside/40 shadow-sm outline-none duration-200 placeholder:text-obsidian-400 group-hover:border-jade-500 sm:text-sm`} disabled={disabled} />
+                    <span className="absolute inset-y-0 right-0 grid w-10 duration-200 pointer-events-none place-content-center text-obsidian-400 group-hover:text-jade-500">
+                        <svg xmlns="http:ww.w3.org/2000/svg" className="" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="12" cy="12" r="4"></circle>
+                            <path d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28"></path>
+                        </svg>
+                    </span>
                     <span className="sr-only">Input</span>
                 </div>
             )
