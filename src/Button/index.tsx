0d589fbcb21100d7ff6dc43a141f8251180fcb74
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-11-14 21:31:32 +0000

    .

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -6,7 +6,7 @@ export const RuiButton = ({ id, form, isLoading, loadingLabel, fullWidth, value,
   switch (variant) {
     case "ghost":
       return (
-        <button id={id} form={form} value={value} type={type} onClick={onClick} {...props} className={`flex active:scale-95 rui-padding ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] bg-transparent ${ghostButtonStyles[color]} text-sm duration-150 disabled:pointer-events-none disabled:border-gray-300 disabled:text-gray-300 disabled:select-none`} disabled={disabled}>
+        <button id={id} form={form} value={value} type={type} onClick={onClick} {...props} className={`flex active:scale-95 rui-padding ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] bg-transparent ${ghostButtonStyles[color]} text-sm duration-150 disabled:pointer-events-none disabled:border-obsidian-300 disabled:text-obsidian-300 disabled:select-none`} disabled={disabled}>
           {isLoading ? (
             <>
               {loadingSpinner}
@@ -24,7 +24,7 @@ export const RuiButton = ({ id, form, isLoading, loadingLabel, fullWidth, value,
       );
     case "text":
       return (
-        <button id={id} form={form} value={value} type={type} onClick={onClick} {...props} className={`flex active:scale-95 rui-padding ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center ${textButtonStyles[color]} gap-3 rounded-[9px] border-[1.5px] border-transparent text-sm backdrop-blur-sm duration-150 disabled:pointer-events-none disabled:text-gray-300 disabled:select-none`} disabled={disabled}>
+        <button id={id} form={form} value={value} type={type} onClick={onClick} {...props} className={`flex active:scale-95 rui-padding ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center ${textButtonStyles[color]} gap-3 rounded-[9px] border-[1.5px] border-transparent text-sm backdrop-blur-sm duration-150 disabled:pointer-events-none disabled:text-obsidian-300 disabled:select-none`} disabled={disabled}>
           {isLoading ? (
             <>
               {loadingSpinner}
@@ -42,7 +42,7 @@ export const RuiButton = ({ id, form, isLoading, loadingLabel, fullWidth, value,
       );
     case "filled":
       return (
-        <button id={id} form={form} value={value} type={type} onClick={onClick} {...props} className={`flex active:scale-95 ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] border-transparent ${filledButtonStyles[color]} rui-padding text-sm text-onyx duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} disabled={disabled}>
+        <button id={id} form={form} value={value} type={type} onClick={onClick} {...props} className={`flex active:scale-95 ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] border-transparent ${filledButtonStyles[color]} rui-padding text-sm duration-150 disabled:pointer-events-none disabled:bg-obsidian-100 disabled:text-obsidian-300 dark:disabled:bg-obsidian-800 dark:disabled:text-obsidian-700 disabled:select-none`} disabled={disabled}>
           {isLoading ? (
             <>
               {loadingSpinner}
@@ -61,16 +61,14 @@ export const RuiButton = ({ id, form, isLoading, loadingLabel, fullWidth, value,
     case "social":
       return (
         <button {...props} onClick={onClick} className={`flex items-center rui-padding outline-none active:scale-95 gap-3 rounded-[9px] border-[1.5px] border-transparent text-onyx duration-200 ${socialColorStyles[socialColor]} ${fullWidth ? 'w-full' : 'w-fit'}`}>
-          <span>{leftIcon}</span>
-          {children && <span className='sm:text-sm'>
+          {children && <span>
             {children}
-          </span>
-          }
+          </span>}
         </button>
       )
     default:
       return (
-        <button id={id} form={form} value={value} type={type} onClick={onClick} {...props} className={`flex active:scale-95 ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] border-transparent ${filledButtonStyles[color]} rui-padding text-sm text-onyx duration-150 disabled:pointer-events-none disabled:bg-gray-100 disabled:text-gray-300 dark:disabled:bg-gray-800 dark:disabled:text-gray-700 disabled:select-none`} disabled={disabled}>
+        <button id={id} form={form} value={value} type={type} onClick={onClick} {...props} className={`flex active:scale-95 ${fullWidth ? 'w-full' : 'w-fit'} items-center justify-center gap-3 rounded-[9px] border-[1.5px] border-transparent ${filledButtonStyles[color]} rui-padding text-sm text-onyx duration-150 disabled:pointer-events-none disabled:bg-obsidian-100 disabled:text-obsidian-300 dark:disabled:bg-obsidian-800 dark:disabled:text-obsidian-700 disabled:select-none`} disabled={disabled}>
           {isLoading ? (
             <>
               {loadingSpinner}
